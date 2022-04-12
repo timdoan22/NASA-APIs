2c6c9e96a21c0bcfd675a92e4b7671464c43551a
timdoan22 <timdoan01@gmail.com>
2022-04-12 04:13:12 -0400

    video integration now displays after fetch response

--- js/main.js
+++ js/main.js
@@ -3,11 +3,20 @@ const NASA_API_KEY = config.NASA_API_KEY
 fetch(`https://api.nasa.gov/planetary/apod?api_key=${NASA_API_KEY}`)
     .then(res => res.json())
     .then(data => {
-        console.log(data)
         document.querySelector('h1').innerText += data.title
         document.querySelector('img').classList.toggle('hidden')
         document.querySelector('img').src = data.hdurl
         document.querySelector('h3').innerText = data.explanation
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    });
+
+fetch('https://images-assets.nasa.gov/video/Space-Exploration-Video-1/collection.json')
+    .then(res => res.json())
+    .then(data => {
+        document.querySelector('iframe').classList.toggle('hidden')
+        document.querySelector('iframe').src = data[4]
     })
     .catch(err => {
         console.log(`error ${err}`)
