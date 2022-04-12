53ee412c0fa5541eff4e459c7e8db9654952de8f
timdoan22 <timdoan01@gmail.com>
2022-04-12 04:03:38 -0400

    fetch response now returns the explanation text

--- js/main.js
+++ js/main.js
@@ -5,6 +5,9 @@ fetch(`https://api.nasa.gov/planetary/apod?api_key=${NASA_API_KEY}`)
     .then(data => {
         console.log(data)
         document.querySelector('h1').innerText += data.title
+        document.querySelector('img').classList.toggle('hidden')
+        document.querySelector('img').src = data.hdurl
+        document.querySelector('h3').innerText = data.explanation
     })
     .catch(err => {
         console.log(`error ${err}`)
